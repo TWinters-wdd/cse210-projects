7abdc6330bde79f8053d17609a5ac399724d4066
HotGarbo04 <talmageawinters@gmail.com>
2026-04-22 14:38:47 -0600

    Comment about console.WriteLine

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,9 @@ class Program
 {
     static void Main(string[] args)
     {
+
+        // This prints a message to the console
+        // another comment
         Console.WriteLine("Hello Sandbox World!");
     }
 }
