992d0e98854bf5bf85b09b00cc93136677b98864
HotGarbo04 <talmageawinters@gmail.com>
2026-04-30 13:48:20 -0600

    C# Prep 3 Code & Sandbox

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+        int intGuess = -1;
+
+        while (intGuess != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            string strGuess = Console.ReadLine();
+            intGuess = int.Parse(strGuess);
+
+            if (intGuess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (intGuess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
+        
     }
 }
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -2,11 +2,74 @@ using System;
 
 class Program
 {
+
+    static void DisplayGreeting()
+    {
+        Console.WriteLine("Hello Betty");
+    }
+
+    static double AddNumbers(int n1, int n2, double n3)
+    {
+        double total = n1 + n2 + n3;
+        return total;
+    }
+
     static void Main(string[] args)
     {
+        DisplayGreeting();
+        double total = AddNumbers(10, 20, 1001.234);
+        Console.WriteLine(total);
 
         // This prints a message to the console
         // another comment
-        Console.WriteLine("Hello Sandbox World!");
+
+        // int x = 10;
+
+        // if (x == 10)
+        // {
+        //     Console.WriteLine("Hey Bob");
+        // }
+
+        // for (int i = 100000; i > -200000; i-= 10000)
+        // {
+        //     Console.WriteLine(i);
+        // }
+
+        // int age = -1;
+
+        // while (age < 0 || age > 125)
+        // {
+        //     Console.WriteLine("Please enter your age: ");
+        //     age = int.Parse(Console.ReadLine());
+        // }
+
+        // int age;
+
+        // do
+        // {
+        //     Console.WriteLine("Please enter your age: ");
+        //     age = int.Parse(Console.ReadLine());
+        // } while (age < 0 || age > 125);
+
+        // for (int i = 0; i <= 32; ++i)
+        // {
+        //     uint powerNumber = (uint)Math.Pow(2, i);
+        //     Console.Write($"2 to the power of {i}, is {powerNumber}");
+        //     Console.WriteLine($", 0x{powerNumber.ToString("X")}");
+        // }
+
+
+        // List<int> myData = new List<int>();
+        // myData.Add(1);
+        // myData.Add(2);
+        // myData.Add(3);
+        // myData.Add(4);
+        // myData.Add(5);
+        // myData.Add(99);
+        // myData.Add(-100);
+        // foreach(int i in myData)
+        // {
+        //     Console.WriteLine(i);
+        // }
     }
 }
