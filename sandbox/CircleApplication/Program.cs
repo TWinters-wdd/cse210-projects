b6ca944d659b3461c53b10284ad77edf21dbf65e
HotGarbo04 <talmageawinters@gmail.com>
2026-05-04 14:59:37 -0600

    Cylinder class added

--- sandbox/CircleApplication/Program.cs
+++ sandbox/CircleApplication/Program.cs
@@ -8,5 +8,18 @@ class Program
         Circle myCircle = new Circle();
         myCircle.SetRadius(10);
         Console.WriteLine(myCircle.GetArea());
+
+        myCircle._radius = 20;
+        Console.WriteLine(myCircle._radius);
+        Console.WriteLine(myCircle.GetArea());
+
+        Circle myCircle2 = new Circle();
+        myCircle2._radius = 30;
+        Console.WriteLine(myCircle2.GetArea());
+
+        Cylinder myCylinder = new Cylinder();
+        myCylinder._circle = new Circle();
+        myCylinder._circle._radius = 10;
+        myCylinder._height = 30;
     }
 }
