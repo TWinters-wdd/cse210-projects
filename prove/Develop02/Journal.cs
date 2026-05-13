f71abdceca5c478dd9b0333c3d8a628a465f5547
HotGarbo04 <talmageawinters@gmail.com>
2026-05-13 14:53:16 -0600

    Have init Journal Class working

--- prove/Develop02/Journal.cs
+++ prove/Develop02/Journal.cs
@@ -1,3 +1,17 @@
-using System;
-using System.Collections.Generic;
+class Journal
+{
+    List<JournalEntry> _journalEntries = new List<JournalEntry>();
 
+    public void AddJournalEntry(JournalEntry journalEntry)
+    {
+        _journalEntries.Add(journalEntry);
+    }
+
+    public void DisplayJournal()
+    {
+        foreach (JournalEntry journalEntry in _journalEntries)
+        {
+            journalEntry.DisplayJournalEntry();
+        }
+    }
+}
--- prove/Develop02/JournalEntry.cs
+++ prove/Develop02/JournalEntry.cs
@@ -4,15 +4,18 @@ class JournalEntry
     string _prompt;
     string _response;
 
+    // Finish this code - need more prompts
     string[] _prompts =
     {
-        "How are you feeling today?",
-        "Who did you talk with today?"
+        "How are you feeling today",
+        "Who did you talk with today"
     };
 
     public void CreateJournalEntry()
     {
         _date = DateTime.Now.ToShortDateString();
+
+        // Finish this code - need a random prompt
         _prompt = _prompts[0];
         Console.WriteLine(_prompts[0]);
         _response = Console.ReadLine();
@@ -21,5 +24,10 @@ class JournalEntry
     public void DisplayJournalEntry()
     {
         Console.WriteLine($"{_date}, {_prompt}, {_response}");
+    }
+
+    public string CreateFileSystemString()
+    {
+        return $"{_date}#{_prompt}#{_response}";
     }
 }
