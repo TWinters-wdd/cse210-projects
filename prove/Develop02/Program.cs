04f5ea21a0fb3501a1cf573c0c7078c7f2dd1987
HotGarbo04 <talmageawinters@gmail.com>
2026-05-16 12:59:14 -0600

    Deleted Unused Code

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -49,18 +49,5 @@ class Program
             }
         } while (userChoice != 6);
 
-
-        // JournalEntry myJournalEntry = new JournalEntry();
-        // myJournalEntry.CreateJournalEntry();
-        // // myJournalEntry.DisplayJournalEntry();
-        // // Console.WriteLine(myJournalEntry.CreateFileSystemString());
-        // JournalEntry myJournalEntry2 = new JournalEntry();
-        // myJournalEntry2.CreateJournalEntry();
-
-        // Journal myJournal = new Journal();
-        // myJournal.AddJournalEntry(myJournalEntry);
-        // myJournal.AddJournalEntry(myJournalEntry2);
-
-        // myJournal.DisplayJournal();
     }
 }
