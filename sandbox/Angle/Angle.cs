acc445d7da5e24ccf9b3bb79c3b5638da01b7f67
HotGarbo04 <talmageawinters@gmail.com>
2026-05-18 15:11:28 -0600

    Sandbox Angle Project Demo

---
+++ sandbox/Angle/Angle.cs
@@ -0,0 +1,16 @@
+class Angle
+{
+    private double _radians;
+
+    public double GetRadians()
+    {
+        return _radians;
+    }
+
+    public void SetRadians(double radians)
+    {
+        _radians = radians;
+    }
+
+
+}
