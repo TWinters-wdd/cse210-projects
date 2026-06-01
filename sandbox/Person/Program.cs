c0135e364854f03ce51274f0afe4289fd62d8c4b
HotGarbo04 <talmageawinters@gmail.com>
2026-06-01 14:59:53 -0600

    Person Class Demo

---
+++ sandbox/Person/Program.cs
@@ -0,0 +1,12 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        Person myPerson = new Person("Bob", "Roberts", 57, 198);
+        Console.WriteLine(myPerson.GetPersonInformation());
+
+        Police myPoliceMan = new Police( "Gun", "Jonny", "Rich", 43, 187);
+
+        Console.WriteLine(myPoliceMan.GetPoliceManInformation());
+    }
+}
