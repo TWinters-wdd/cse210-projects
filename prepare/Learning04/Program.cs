f6dc249c8f9390a5e9c16fb05cd07c65b6b9bdea
HotGarbo04 <talmageawinters@gmail.com>
2026-06-01 11:37:44 -0600

    W07 Prepare MathAssignment Class Created and Tested

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -7,5 +7,10 @@ class Program
         Assignment testAssignment = new Assignment("Samuel Bennet", "Multiplication");
 
         Console.WriteLine(testAssignment.GetSummary());
+
+        MathAssignment testMathAssignment = new MathAssignment("Roberto Rodriguez", "Fractions", "Section 7.3", "Problems 8-19");
+
+        Console.WriteLine(testMathAssignment.GetSummary());
+        Console.WriteLine(testMathAssignment.GetHomeworkList());
     }
 }
