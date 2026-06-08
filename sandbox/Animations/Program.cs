68805b3f7343da84e02c484a2f76a2a0dddfcee2
HotGarbo04 <talmageawinters@gmail.com>
2026-06-08 17:20:51 -0600

    Animation example from class

---
+++ sandbox/Animations/Program.cs
@@ -0,0 +1,33 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        
+        int sleepTime = 250;
+
+        Console.Clear();
+        Console.CursorVisible = false;
+        // for (int i = 0; i < 20; i++)
+        // {
+        //     Console.Write("+");
+        //     Thread.Sleep(sleepTime);
+        //     Console.Write("\b");
+        //     Console.Write("-");
+        //     Thread.Sleep(sleepTime);
+        //     Console.Write("\b");
+        // }
+        // Console.CursorVisible = true;
+
+
+        string animationString = "-\\|/";
+
+        for (int i = 0; i < 20; i++)
+        {
+            Console.Write(animationString[i % animationString.Length]);
+            Thread.Sleep(sleepTime);
+            Console.Write("\b");
+        }
+        Console.CursorVisible = true;
+
+    }
+}
