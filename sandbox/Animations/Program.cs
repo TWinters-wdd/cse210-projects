df1df61c7c2e98bb1300a5958d0d071c71a3301d
HotGarbo04 <talmageawinters@gmail.com>
2026-06-10 14:25:13 -0600

    Animation Demo

--- sandbox/Animations/Program.cs
+++ sandbox/Animations/Program.cs
@@ -3,7 +3,7 @@
     public static void Main(string[] args)
     {
         
-        int sleepTime = 250;
+        // int sleepTime = 250;
 
         Console.Clear();
         Console.CursorVisible = false;
@@ -19,13 +19,28 @@
         // Console.CursorVisible = true;
 
 
-        string animationString = "-\\|/";
+        // string animationString = "-\\|/";
+        // DateTime now = DateTime.Now;
+        // DateTime endTime = now.AddSeconds(10);
 
-        for (int i = 0; i < 20; i++)
+        // // for (int i = 0; i < 20; i++)
+        // int index = 0;
+        // while (DateTime.Now < endTime)
+        // {
+        //     Console.Write(animationString[index++ % animationString.Length]);
+        //     Thread.Sleep(sleepTime);
+        //     Console.Write("\b");
+        // }
+
+        int count = 15;
+        DateTime now = DateTime.Now;
+        DateTime endTime = now.AddSeconds(count);
+
+        while (count >= 0)
         {
-            Console.Write(animationString[i % animationString.Length]);
-            Thread.Sleep(sleepTime);
-            Console.Write("\b");
+            Console.Write($"{count--,2}");
+            Thread.Sleep(1000);
+            Console.Write("\b\b");
         }
         Console.CursorVisible = true;
 
