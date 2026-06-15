fba386c035a764dbdd4addf4691a17f15a43eaa2
HotGarbo04 <talmageawinters@gmail.com>
2026-06-15 11:01:13 -0600

    Shape Class Created, Square Class Created and Tested

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,9 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Square mySquare = new Square("Purple", 10);
+
+        Console.WriteLine(mySquare.GetColor());
+        Console.WriteLine(mySquare.GetArea());
     }
 }
