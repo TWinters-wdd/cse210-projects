6e4c89102026be9b8a9e53a13ed71f688370881c
HotGarbo04 <talmageawinters@gmail.com>
2026-06-15 11:15:18 -0600

    Circle Class Created and Tested, Shape List Created

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -14,5 +14,23 @@ class Program
         Rectangle myRectangle = new Rectangle("Orange", 30, 5);
         Console.WriteLine(myRectangle.GetColor());
         Console.WriteLine(myRectangle.GetArea());
+
+        Console.WriteLine("");
+
+        Circle myCircle = new Circle("Pink", 2);
+        Console.WriteLine(myCircle.GetColor());
+        Console.WriteLine(myCircle.GetArea());
+
+        List<Shape> shapeList = new List<Shape>();
+        shapeList.Add(mySquare);
+        shapeList.Add(myRectangle);
+        shapeList.Add(myCircle);
+
+        Console.WriteLine("\nList of Shapes:");
+        foreach (Shape shape in shapeList)
+        {
+            Console.WriteLine(shape.GetColor());
+            Console.WriteLine(shape.GetArea());
+        }
     }
 }
