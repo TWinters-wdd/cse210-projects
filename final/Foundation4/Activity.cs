73a1ff4ba75e2f49bd06bbed40ea581294881ce6
HotGarbo04 <talmageawinters@gmail.com>
2026-07-15 14:51:27 -0600

    Foundation 4 Base Activity Class Created and Tested

---
+++ final/Foundation4/Activity.cs
@@ -0,0 +1,20 @@
+public class Activity
+{
+    private string _date;
+    private float _duration;
+
+    public Activity(string date, float duration)
+    {
+        _date = date;
+        _duration = duration;
+    }
+
+    // public abstract float GetDistance();
+    // public abstract float GetSpeed();
+    // public abstract float GetPace();
+
+    public virtual string GetSummary()
+    {
+        return $"{_date} ({_duration} min)";
+    }
+}
--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -1,9 +1,9 @@
-using System;
-
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation4 World!");
+        Activity testActivity = new Activity("02/14/2026", 30);
+
+        Console.WriteLine(testActivity.GetSummary());
     }
 }
