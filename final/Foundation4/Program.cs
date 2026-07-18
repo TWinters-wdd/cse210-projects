c2fd9a5db773e0cbae1e08d8d049f3c54c2e3cab
HotGarbo04 <talmageawinters@gmail.com>
2026-07-18 11:54:40 -0600

    Foundation 4 Project Completed

--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -2,12 +2,20 @@ class Program
 {
     static void Main(string[] args)
     {
+        List<Activity> exercises = new List<Activity>();
+
         RunningActivity runningActivity = new RunningActivity("03 Nov 2022", 30, 3);
         BikingActivity bikingActivity = new BikingActivity("25 July 2026", 45, 25);
         SwimmingActivity swimmingActivity = new SwimmingActivity("01 Aug 2026", 60, 20);
 
-        // Console.WriteLine(runningActivity.GetSummary());
-        // Console.WriteLine(bikingActivity.GetSummary());
-        Console.WriteLine(swimmingActivity.GetSummary());
+        exercises.Add(runningActivity);
+        exercises.Add(bikingActivity);
+        exercises.Add(swimmingActivity);
+
+        foreach (Activity exercise in exercises)
+        {
+            Console.WriteLine(exercise.GetSummary());
+            Console.WriteLine();
+        }
     }
 }
